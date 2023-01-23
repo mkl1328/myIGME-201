34597f17d3a765e69b70cb11989c50c005ae0e53
Max <mkl1328@rit.edu>
2023-01-23 11:30:07 -0500

    Session 2-1

---
+++ FavoriteColorAndNumber/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorNameSpace
+{
+    namespace Color
+    {
+        static class ColorClass
+        {
+            public static string sFavColor = null;
+        }
+    }
+}
+
+
+namespace FavoriteColorAndNumber
+{
+    using CAlias = ColorNameSpace.Color;
+    // Class: Program
+    // Author: Max Lama
+    // Purpose: Console Read/Write and Exception-handling exercise
+    // Restrictions: None
+    static internal class Program
+    {
+        // Method: Main
+        // Purpose: Prompt user for thier favorite number and color, and outputs that to the console.
+        //          Outputs favorite color, favorite number of times                  
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            //int favNum = 0;
+            int nFavNum = 0;
+            string sFavNum = null;
+            sFavNum = "123";
+
+            int? npFavNum = null;
+            //      I dont understand the pointer null thing???
+
+
+            // Prompt user for fav color
+            Console.Write("Enter your favorite color:\t");
+
+            // read from console and store into variable
+            CAlias.ColorClass.sFavColor = Console.ReadLine();
+
+            // Prompt user for favorite number
+            Console.Write("Enter your favorite number:\t");
+            
+            // Check for int input
+            
+            /*
+            do
+            {
+
+                // read from console and store into variable
+                sFavNum = Console.ReadLine();
+
+                try
+                {
+                   npFavNum = Convert.ToInt32(sFavNum);
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter an integer.");
+                }
+            } while(npFavNum == null);
+            */
+
+            if (int.TryParse(sFavNum, out nFavNum)) nFavNum = int.Parse(sFavNum);
+
+            // Change text to match their color
+
+            switch(CAlias.ColorClass.sFavColor.ToLower())
+            {
+                case "red":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+
+                case "green":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                     break;
+
+                case "blue":
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+
+                default:
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    break;
+
+            }
+
+            // Output their favorite color - fav number of times
+            for (int i = 0; i < nFavNum; i++)
+            {
+                Console.Write(CAlias.ColorClass.sFavColor);
+            }
+
+            //Diff string output:
+            //String appending -    ("..." + var)
+            //String Embedding -    ($"... + ()")
+            //String substitution - ("... {0}", var)
+
+        }
+    }
+}
---
+++ Lama_PE01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lama_PE01
+{
+    // Class: Program
+    // Author: Max Lama
+    // Purpose: Practice Exercise 1 - Print hello world 
+    // Restrictions: None
+    static internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Max Lama");
+
+            var x = 10;
+            x += 100;
+            Console.WriteLine(x);
+
+            if(x == 10) {
+                Console.WriteLine(x + " = 10");
+            }
+        }
+    }
+}
