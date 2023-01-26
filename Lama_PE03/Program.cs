d1826a62486e8a337f45cc03202547e511d9bd9f
Max <mkl1328@rit.edu>
2023-01-26 04:06:29 -0500

    PE03 - Question 5 program

---
+++ Lama_PE03/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE03
+{
+    // Class: Program
+    // Author: Max Lama
+    // Purpose: Collect 4 numbers from user input, and output the product
+    // Restrictions: None
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double total = 1;
+            // For loop for collecting 4 doubles   
+            for (int i = 0; i < 4; i++)
+            {
+                bool isNum = false;
+                // Data Validation
+                while (!isNum) 
+                {
+                    try
+                    {
+                        Console.WriteLine($"Please enter in a number. ({i}/4)");
+                        double temp = Convert.ToDouble(Console.ReadLine());
+                        
+                        // Multiply them all Each time collected
+                        total *= temp;
+                        isNum = true;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Please enter a valid number value.");
+                    }
+                }
+                
+            }
+            // Write product to console
+            Console.WriteLine($"The product of those four numbers is: {total}");
+        }
+    }
+}
