bde25159d12fa740bd6efd2dd2b5407bd24838d1
Max <mkl1328@rit.edu>
2023-02-07 18:25:11 -0500

    PE08 - Q7

---
+++ PE08-q7/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE08_q7
+{
+    // Author: Max Lama
+    // Class: Program
+    // Purpose: Question 7 - reverse a string.
+    // Restrictions: None
+    internal class Program
+    {
+        // Author: Max Lama
+        // Purpose: same as Program
+        static void Main(string[] args)
+        {
+            string input = "";
+            string reverse = "";
+
+            //Prompt for string
+            Console.WriteLine("Give me a line to reverse!");
+            //Get string
+            input = Console.ReadLine();
+
+            //Loop through string and add it to a new "Reverse string" variable
+            for (int i = input.Length; i > 0; i--)
+            {
+                reverse += input[i-1];
+            }
+            
+            //print to console.
+            Console.WriteLine($"Here's the reversed string: \n{reverse}");
+        }
+    }
+}
