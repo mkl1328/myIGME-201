5b0992395866da7bcb0c28a072aaf2925ebe0295
Max Lama <98444425+mkl1328@users.noreply.github.com>
2023-02-09 20:12:24 +0000

    begun: icon class

--- Final-Project/Mockup/Icon.js
+++ Final-Project/Mockup/Icon.js
@@ -1,5 +1,23 @@
+/**
+ * @author Max Lama
+ */
 class Icon {
-  constructor() {
-    
+  /**
+   * 
+   * @param {} typeDef 
+   */
+  constructor(typeDef) {
+    this.pos = new createVector(0, 0);
+    this.states = ["welcome", "selected", "unselected"];
+    this.scale = new createVector(100,100);
+    this.type = typeDef || "circle"
   }
+
+  display() {
+    switch(this.type) {
+      case "square" :
+        break;
+    }
+  }
+
 }
