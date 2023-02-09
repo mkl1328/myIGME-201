346dd8c62976562e173b3f2bfed3399bbee59a16
Max Lama <98444425+mkl1328@users.noreply.github.com>
2023-02-09 09:27:44 +0000

    Init comments

--- Final-Project/Mockup/sketch.js
+++ Final-Project/Mockup/sketch.js
@@ -1,7 +1,30 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1000, 600);
 }
 
 function draw() {
   background(220);
 }
+
+/**
+ * Objs- 
+ *  - Icon
+ *     - Barn
+ *     - Profile
+ *  - Map
+ *     - Wall ( clickable) 
+ *       - Route.
+ *  - Stat Module
+ * 
+ * Screens:
+ *  - Welcome
+ *      -- click on one of the icons to start
+ *         icons glide to the sidebar, transition to page
+ *  - Maps
+ *  - Profile
+ * 
+ * Animations:
+ *  - welcome glide
+ *  - scroll down old page + fade in / size up new page
+ */
+
