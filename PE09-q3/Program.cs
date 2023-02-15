c2db46f5165fa0ef74d0b73514afd58c7b483541
Max <mkl1328@rit.edu>
2023-02-14 19:07:29 -0500

    I don't think this is right

---
+++ PE09-q3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE09_q3
+{
+    // Class: Program
+    // Author: Max Lama
+    // Purpose: delgate function
+    // Restrictions: none
+    internal class Program
+    {
+
+        // Method: Main
+        // Purpose: Hold Delegate function impersonating ReadLine()
+        // Restrictions: it only returns a blank string.
+        static void Main(string[] args)
+        {
+            Func<string> fakeReadLine;
+
+            fakeReadLine = () =>
+            {
+                string returnVal = ""; // How do I acquire user input without using readline????
+                return returnVal;
+            };
+        }
+    }
+}
