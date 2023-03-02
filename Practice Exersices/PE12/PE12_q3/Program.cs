794183e91405aa41740a34b2cdf6c4ba2ca08ee1
Max <mkl1328@rit.edu>
2023-03-01 21:32:29 -0500

    q3

---
+++ Practice Exersices/PE12/PE12_q3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE12_q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class MyClass
+    {
+        private string myString;
+        public virtual string GetString()
+        {
+            return this.myString;
+        }
+
+        public string MyString
+        {
+            set
+            {
+                this.myString = value;
+            }
+        }
+    }
+
+    public class MyDerivedClass : MyClass
+    {
+        public override string GetString() 
+        {
+            return base.GetString() + " (output from the derived class)";
+        }
+    }
+}
